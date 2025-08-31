ac08ea83a096f3921661bd1b270daaa3b9ccac2c
DaoChaShao <27882272@qq.com>
2025-08-31 23:09:18 +0800

    feat: add __init__.py with main function template

---
+++ utils/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/8/31 22:51
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   __init__.py.py
+# @Desc     :   
+
+def main() -> None:
+    """ Main Function """
+    pass
+
+
+if __name__ == "__main__":
+    main()
