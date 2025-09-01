81b56aea6356371be0ba727bec3cfdcfcf667e12
DaoChaShao <27882272@qq.com>
2025-09-01 23:18:11 +0800

    feat: add advanced_test.py for enhanced testing capabilities

---
+++ subpages/advanced_test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/1 23:15
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   advanced_test.py
+# @Desc     :
