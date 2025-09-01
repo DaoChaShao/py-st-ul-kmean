2f753dbdb3bb0fd9d996dbb822df6db43f9914e3
DaoChaShao <27882272@qq.com>
2025-09-01 23:18:15 +0800

    feat: add advanced_train.py for initial training functionality

---
+++ subpages/advanced_train.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/1 23:14
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   advanced_train.py
+# @Desc     :   
+
+
